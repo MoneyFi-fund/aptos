5562ed33b3cfe9c5eee6fe13cca54c6ee5b23950
thnt <coder53@gmail.com>
2025-07-08 11:01:57 +0700

    chore: update deps setup

--- contracts/setup.sh
+++ contracts/setup.sh
@@ -7,6 +7,9 @@ HYPERION_ADDRESS="0x8b4a2c4bb53857c718a04c020b98f8c2e1f99a68b0f57389a8bf5434cd22
 
 set -e
 
+CWD=$(dirname $(realpath "$0"))
+cd "$CWD"
+
 function download_package() {
     local DIR=""
     local PKG=""
